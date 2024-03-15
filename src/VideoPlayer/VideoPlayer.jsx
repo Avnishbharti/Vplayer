5dd60c510e28c5e105752b2371104deff7b01299
avnishkumarB <avnish@kalibre.ai>
2024-03-15 14:55:50 +0530

    custom control changes

--- src/VideoPlayer/VideoPlayer.jsx
+++ src/VideoPlayer/VideoPlayer.jsx
@@ -1,15 +1,21 @@
 import React, { useEffect, useRef, useState } from "react";
 import { videosData } from "./data";
 import PlayList from "./PlayList";
+import { Icon } from "@iconify/react";
 
 const VideoPlayer = () => {
   const videoRef = useRef(null);
 
   const [videoData, setVideoData] = useState([]);
+  const [isPlay, setIsPlay] = useState(false);
   const [currentVideoData, setCurrentVideoData] = useState({
     index: 0,
     info: videosData[0],
   });
+
+  const [volume, setVolume] = useState(0.5);
+  const [playbackRate, setPlaybackRate] = useState(1);
+  const [seekPosition, setSeekPosition] = useState(0);
   useEffect(() => {
     setVideoData(videosData);
   }, []);
@@ -18,8 +24,10 @@ const VideoPlayer = () => {
     const video = videoRef.current;
     if (video.paused) {
       video.play();
+      setIsPlay(true);
     } else {
       video.pause();
+      setIsPlay(false);
     }
   };
 
@@ -30,6 +38,98 @@ const VideoPlayer = () => {
     setShowFullText(!showFullText);
   };
 
+  const handleVolumeChange = (e) => {
+    const newVolume = e.target.value / 100;
+    setVolume(newVolume);
+    videoRef.current.volume = newVolume;
+  };
+
+  const handlePlaybackRateChange = (e) => {
+    const newPlaybackRate = parseFloat(e.target.value);
+    console.log("jhddjhfuhdfoueodfobhjk", newPlaybackRate);
+    setPlaybackRate(newPlaybackRate);
+    videoRef.current.playbackRate = newPlaybackRate;
+  };
+
+  useEffect(() => {
+    const video = videoRef.current;
+    if (video) {
+      video.addEventListener("timeupdate", () => {
+        const newSeekPosition = (video.currentTime / video.duration) * 100;
+        setSeekPosition(newSeekPosition);
+      });
+    }
+
+    return () => {
+      if (video) {
+        video.removeEventListener("timeupdate", () => {});
+      }
+    };
+  }, [videoRef]);
+
+  const handleSeeking = (e) => {
+    const newCurrentTime = e.target.value / 100;
+    videoRef.current.currentTime = newCurrentTime * videoRef.current.duration;
+    setSeekPosition(e.target.value);
+  };
+
+  const getFormattedTime = (timeInSeconds) => {
+    const minutes = Math.floor(timeInSeconds / 60);
+    const seconds = Math.floor(timeInSeconds % 60);
+    return `${minutes.toString().padStart(2, "0")}:${seconds
+      .toString()
+      .padStart(2, "0")}`;
+  };
+
+  const videoDuration = videoRef.current?.duration;
+  const currentVideoTime = videoRef.current?.currentTime;
+
+  const [isFullScreen, setIsFullScreen] = useState(false);
+
+  const [previousWidth, setPreviousWidth] = useState(null);
+  const [previousHeight, setPreviousHeight] = useState(null);
+
+  const toggleFullScreen = () => {
+    if (!isFullScreen) {
+      setPreviousWidth(videoRef.current.offsetWidth);
+      setPreviousHeight(videoRef.current.offsetHeight);
+      videoRef.current.requestFullscreen();
+    } else {
+      document.exitFullscreen();
+    }
+    setIsFullScreen(!isFullScreen);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (isFullScreen) {
+        // const videoWidth = videoRef.current.videoWidth;
+        // const videoHeight = videoRef.current.videoHeight;
+        // const containerWidth = videoRef.current.parentElement.offsetWidth;
+        // const containerHeight = videoRef.current.parentElement.offsetHeight;
+
+        // // Calculate optimal scaling based on aspect ratio
+        // const widthScale = containerWidth / videoWidth;
+        // const heightScale = containerHeight / videoHeight;
+        // const scale = Math.min(widthScale, heightScale); // Maintain aspect ratio
+
+        // videoRef.current.style.transform = `scale(${scale})`;
+      } else {
+        if (previousWidth && previousHeight) {
+          videoRef.current.style.transform = `scale(1)`; // Reset scale for non-fullscreen
+          videoRef.current.style.width = `${previousWidth}px`; // Set previous width
+          videoRef.current.style.height = `${previousHeight}px`; // Set previous height
+        }
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [isFullScreen, previousWidth, previousHeight]);
+
   return (
     <div className="h-v w-screen">
       <div className="w-full flex justify-center">
@@ -39,7 +139,7 @@ const VideoPlayer = () => {
         <div className="md:col-span-1">
           <video
             src={currentVideoData?.info?.sources}
-            controls
+            // controls
             autoPlay
             width={"99%"}
             // height={"700px"}
@@ -75,8 +175,78 @@ const VideoPlayer = () => {
               });
             }}
           />
+          {/* <div className="rounded-md bg-slate-200 p-2 mt-2 text-start w-description">
+            <button onClick={togglePlayPause}>
+              {isPlay ? (
+                <Icon icon="fluent:pause-24-regular" height={30} width={30} />
+              ) : (
+                <Icon icon="fluent:play-24-regular" height={30} width={30} />
+              )}
+            </button>
+          </div> */}
 
-          <div className="rounded-md bg-slate-200 p-6 mt-2 text-start w-description">
+          <div className="w-description flex items-center justify-between p-1 gap-2 bg-slate-300 rounded-b-md">
+            <button onClick={togglePlayPause}>
+              {isPlay ? (
+                <Icon icon="fluent:pause-24-regular" height={30} width={30} />
+              ) : (
+                <Icon icon="fluent:play-24-regular" height={30} width={30} />
+              )}
+            </button>
+
+            <div className="">
+              <h3 className="text-center text-sm">
+                {getFormattedTime(currentVideoTime)}/
+                {getFormattedTime(videoDuration)}
+              </h3>
+            </div>
+
+            <input
+              type="range"
+              min="0"
+              max="100"
+              value={seekPosition} // Convert current time to range value (0-100)
+              onChange={handleSeeking}
+              className="ml-4 w-full" // Adjust width as needed
+            />
+
+            {/* Volume control */}
+            <div className="flex gap-1">
+              <h4 className="text-center text-sm">volume</h4>
+              <input
+                type="range"
+                min="0"
+                max="100"
+                value={volume * 100}
+                onChange={handleVolumeChange}
+                className="w-14"
+              />
+            </div>
+            {/* Playback rate control */}
+            <div className="ml-4 w-40 ">
+              <label htmlFor="playbackRate">speed:</label>
+              <select
+                id="playbackRate"
+                value={playbackRate}
+                onChange={handlePlaybackRateChange}
+              >
+                <option value="0.5">0.5x</option>
+                <option value="1.0" selected>
+                  1.0x (Normal)
+                </option>
+                <option value="1.5">1.5x</option>
+                <option value="2.0">2.0x</option>
+              </select>
+            </div>
+            {/* Fullscreen button (optional) */}
+            <button className="" onClick={toggleFullScreen}>
+              <Icon icon="fluent:page-fit-24-regular" height={30} width={30} />
+            </button>
+          </div>
+
+          {/* This is the end of the design  */}
+
+          <div className="rounded-md bg-slate-200 p-3 mt-2 text-start w-description">
             <h3
               className={`text-gray-600 ${showFullText ? "" : "truncate"}`}
               style={{ maxWidth: "100%" }}
